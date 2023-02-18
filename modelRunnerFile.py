41b4f3d385d08cbdf85f711bfe2db690df11c952
fergy-custom <fergyfive55@gmail.com>
2023-02-18 07:24:06 -0500

    upd

--- modelRunnerFile.py
+++ modelRunnerFile.py
@@ -6,7 +6,9 @@ M.setInputDim( numberOfInputs=2 )
 M.setLossFunc( 'mse' )
 
 M.addLayer( 20, 'tanh' )
+M.addLayer( 20, 'tanh' )
+M.addLayer( 10, 'tanh' )
 M.addLayer( 10, 'tanh' )
 M.addLayer( 1, 'sigmoid' )
 
-M.train( [ [ 1.1, 0.4 ] ], [ 0.5 ] )
+M.train( [ [ 1.1, 0.4 ] ], [ 0.5 ], epochs=100, lr=0.9 )
