cfda281eab70c1dbc7abb5bdd254114b52ee8a91
Eli-Ferguson-Pro-X <elijuddferguson@gmail.com>
2023-02-20 15:16:56 -0500

    Add comments

--- modelRunnerFile.py
+++ modelRunnerFile.py
@@ -9,12 +9,16 @@ M.addLayer( 20, 'sigmoid' )
 M.addLayer( 10, 'sigmoid' )
 M.addLayer( 1, 'sigmoid' )
 
+# Generate a list of random numbers between 0 and 1
+# Round these numbers to get classes, either 0 or 1
 x = [ [xi] for xi in generateListOfNums(200) ]
 y = [ round(xi[0]) for xi in x ]
 print(f'\n\nx:{x}\n\ny:{y}\n\n')
 
+# Train Model
 M.train( x, y, batchSize=5, epochs=100, lr=0.9 )
 
+# Show Predictions
 print('\n\n')
 for xi in x[-5:] :
     print(f'Prediction on {xi}:{ [ round( pred, 2 ) for pred in M.ForwardPropAllLayers( [ xi ] )[0] ]}' )
