574e81cf542ed37bcebc9012e3501c77e0293c4d
fergy-custom <fergyfive55@gmail.com>
2023-02-22 20:03:03 -0500

    upd numbers

--- modelRunnerFile.py
+++ modelRunnerFile.py
@@ -6,17 +6,17 @@ M.setInputDim( numberOfInputs=1 )
 M.setLossFunc( 'binaryCrossEntropy' )
 
 # M.addLayer( 20, 'sigmoid' )
-M.addLayer( 2, 'sigmoid' )
+M.addLayer( 10, 'sigmoid' )
 M.addLayer( 1, 'sigmoid' )
 
 # Generate a list of random numbers between 0 and 1
 # Round these numbers to get classes, either 0 or 1
-x = [ [xi] for xi in generateListOfNums(2) ]
+x = [ [xi] for xi in generateListOfNums(1000) ]
 y = [ round(xi[0]) for xi in x ]
 print(f'\n\nx:{x}\n\ny:{y}\n\n')
 
 # Train Model
-M.train( x, y, batchSize=1, epochs=1, lr=0.9 )
+M.train( x, y, batchSize=1, epochs=10, lr=0.1 )
 
 M.verbose = 0
 
